4367d6524785dd7a90e370777872af44b168db1f
Mozhdeh Marvi <116106151+mzhd10@users.noreply.github.com>
2024-04-28 17:51:55 +0430

    Update index.js

--- index.js
+++ index.js
@@ -31,25 +31,25 @@ let posts = [
     id: 2,
     title: "Serene Blossoms(Kabul) collection",
     body: "Experience the tranquility of nature with our Serene Blossoms Collection. Delicate floral prints and soft pastel hues transport you to a serene garden blooming with beauty and grace. Wrap yourself in the gentle embrace of nature's charm and let your inner beauty blossom.",
-    imageUrl: "uploads/2.png",
+    imageUrl: "uploads/2.PNG",
   },
   {
     id: 3,
     title: "Urban Chic(Mazar) collection",
     body: "Embrace modern sophistication with our Urban Chic Collection. Sleek lines, bold patterns, and vibrant colors come together to redefine hijab fashion for the contemporary woman. From bustling city streets to trendy cafes, exude confidence and style with every step.",
-    imageUrl: "uploads/3.png",
+    imageUrl: "uploads/3.PNG",
   },
   {
     id: 4,
     title: "Golden Sands(Farah) collection",
     body: "Discover the allure of exotic destinations with our Golden Sands Collection. Inspired by sun-kissed beaches and desert landscapes, each hijab in this collection captures the warmth of golden sunsets and the shimmering sands. Embark on a journey of elegance and luxury with every wear.",
-    imageUrl: "uploads/4.png",
+    imageUrl: "uploads/4.PNG",
   },
   {
     id: 5,
     title: "Timeless Classics(Badakhshan) collection",
     body: "ndulge in timeless elegance with our Classics Collection. Featuring timeless designs and rich, luxurious fabrics, these hijabs exude sophistication and grace. Perfect for any occasion, elevate your wardrobe with pieces that stand the test of time and never go out of style.",
-    imageUrl: "uploads/5.png",
+    imageUrl: "uploads/5.PNG",
   },
 ];
 app.set("view engine", "ejs");
